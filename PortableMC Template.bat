25685c8a705044250377b5ae3177649cd7cc5e82
ANTHONUS <67539559+ANTHONUSS@users.noreply.github.com>
2024-09-21 13:55:59 +0200

    Update PortableMC Template.bat
    Update the template to install PortableMC each time the script launches

--- PortableMC Template.bat
+++ PortableMC Template.bat
@@ -1,31 +1,32 @@
 @echo off
 setlocal
 
-rem Chemin complet vers portablemc
-set portablemc_path=%appdata%\Python\Python39\Scripts\portablemc.exe
+pip install portablemc --target="%CD%\PortableMC"
+set PYTHONPATH=%CD%\PortableMC
+set portablemc_path=PortableMC\bin\portablemc.exe
 
-rem Variables
-set email=
-set path=
-set ver=
-set ram=
+rem variables
+    set mc_path=%CD%
+    set user_path=%CD%
 
-rem Verification du portablemc_path
-if not exist %portablemc_path% (
-    echo Le chemin %portablemc_path% n'existe pas. Veuillez verifier l'installation.
-    PAUSE
-    exit /b 1
-)
+    rem Verification of path
+        if not exist "%mc_path%" (
+            echo the path %mc_path% doesn't exist.
+            PAUSE
+            exit /b 1
+        )
+        if not exist "%user_path%" (
+            echo the path %user_path% doesn't exist.
+            PAUSE
+            exit /b 1
+        )
+        
+    set ver=
+    set ram=
+    set email=
 
-rem Verification du path
-if not exist "%path%" (
-    echo Le chemin specifie n'existe pas. Veuillez verifier que le dossier existe bien.
-    PAUSE
-    exit /b 1
-)
+rem execute with jvm args
+    %portablemc_path% --main-dir="%mc_path%" --work-dir="%user_path%" start %ver% -l="%email%" --jvm-args="-Xmx%ram%G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M"
 
-rem Execute portablemc avec les arguments
-%portablemc_path% --main-dir="%path%" --work-dir="%path%" start %ver% -l="%email%" --jvm-args="-Xmx%ram%G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M"
-
-PAUSE
+pause
 endlocal
